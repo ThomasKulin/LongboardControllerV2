38df5b2898c76e8b3d7eb71753d34d3a18f9e00e
Thomas <thomas.kulin@yahoo.ca>
2018-03-30 16:05:00 -0400

    Settings in a ListView. Weird error where entered values are appearing in the wrong locations when scrolling

--- app/src/main/java/com/example/thomas/vesccontroller/Activities/Vesc_Settings_Activity.java
+++ app/src/main/java/com/example/thomas/vesccontroller/Activities/Vesc_Settings_Activity.java
@@ -16,6 +16,8 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -52,20 +54,33 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
          * LIST VIEW
          */
         settingsListView = (ListView) findViewById(R.id.vesc_settings_list);
-        final ListViewItem[] items = new ListViewItem[40];
+        final ListViewItem[] items = new ListViewItem[16];
+        int i = 0;
 
-        items[0] = new ListViewItem("Title " + i,"hint", CustomAdapter.TYPE_TITLE);
-        items[1] = new ListViewItem("EditText " + i,"hint", CustomAdapter.TYPE_EDITTEXT);
-        items[2] = new ListViewItem("CheckBox " + i,"hint", CustomAdapter.TYPE_CHECKBOX);
+        String[] temp = {"BLDC", "FOC", "DC"};
+        items[i++] = new ListViewItem("Motor Type",null,temp, CustomAdapter.TYPE_RADIO);
 
-        CustomAdapter customAdapter = new CustomAdapter(this, R.id.text, items);
+        items[i++] = new ListViewItem("Current",null, null, CustomAdapter.TYPE_TITLE);
+        items[i++] = new ListViewItem("Motor Max","Max motor current (A)", null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Motor Brake Max","Max braking current (A)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Battery Max","Max battery current (A)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Battery Min (regen)","Max regen braking current (A)",null, CustomAdapter.TYPE_EDITTEXT);
+
+        items[i++] = new ListViewItem("Voltage",null,null, CustomAdapter.TYPE_TITLE);
+        items[i++] = new ListViewItem("Voltage Max","Max input voltage (V)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Voltage Min","Min input voltage (V)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Battery Cutoff Start","Threshold to start reducing motor current (V)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Battery Cutoff End","Threshold to cutoff motor current (V)",null, CustomAdapter.TYPE_EDITTEXT);
+
+        items[i++] = new ListViewItem("Temperature",null,null, CustomAdapter.TYPE_TITLE);
+        items[i++] = new ListViewItem("MOSFET Temp Cutoff Start","Threshold to start reducing motor current (°C)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("MOSFET Temp Cutoff End","Threshold to cutoff motor current (°C)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Motor Temp Cutoff Start","Threshold to start reducing motor current (°C)",null, CustomAdapter.TYPE_EDITTEXT);
+        items[i++] = new ListViewItem("Motor Temp Cutoff End","Threshold to cutoff motor current (°C)",null, CustomAdapter.TYPE_EDITTEXT);
+
+
+        CustomAdapter customAdapter = new CustomAdapter(this,R.id.text, items);
         settingsListView.setAdapter(customAdapter);
-        settingsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getBaseContext(), items[i].getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
     @Override
     protected void onResume(){
@@ -85,10 +100,12 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
         private String text;
         private String hint;
         private int type;
+        private String[] radios;
 
-        public ListViewItem(String text, String hint, int type) {
+        public ListViewItem(String text, String hint, String[] radios, int type) {
             this.text = text;
             this.hint = hint;
+            this.radios = radios;
             this.type = type;
         }
 
@@ -101,11 +118,19 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
         }
 
         public String getHint() {
-            return text;
+            return hint;
         }
 
-        public void setHing(String hint) {
+        public void setHint(String hint) {
             this.hint = hint;
+        }
+
+        public String[] getRadios() {
+            return radios;
+        }
+
+        public void setRadios(String[] radios) {
+            this.radios = radios;
         }
 
         public int getType() {
@@ -121,12 +146,13 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
         public static final int TYPE_TITLE = 0;
         public static final int TYPE_EDITTEXT = 1;
         public static final int TYPE_CHECKBOX = 2;
+        public static final int TYPE_RADIO = 3;
 
         private ListViewItem[] objects;
 
         @Override
         public int getViewTypeCount() {
-            return 3;
+            return 4;
         }
 
         @Override
@@ -149,30 +175,62 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
 
             if (convertView == null) {
 
-                if (listViewItemType == TYPE_TITLE) {
-                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_title_item, null);
-                    TextView textView = (TextView) convertView.findViewById(R.id.TV_title_item);
-                    viewHolder = new ViewHolder(textView);
-                } else if (listViewItemType == TYPE_EDITTEXT) {
-                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_text_item, null);
-                    TextView textView = (TextView) convertView.findViewById(R.id.TV_text_item);
-                    EditText editText = (EditText) convertView.findViewById(R.id.text_item_value);
-                    viewHolder = new ViewHolder(textView, editText);
-                } else if (listViewItemType == TYPE_CHECKBOX) {
-                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_checkbox_item, null);
-                    TextView textView = (TextView) convertView.findViewById(R.id.TV_checkbox_item);
-                    CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox_item_checkbox);
-                    viewHolder = new ViewHolder(textView, checkBox);
+                switch(listViewItemType) {
+                    case TYPE_TITLE: {
+                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_title_item, null);
+                        TextView textView = (TextView) convertView.findViewById(R.id.TV_title_item);
+
+                        viewHolder = new ViewHolder(textView);
+                        convertView.setTag(viewHolder);
+                        viewHolder.getText().setText(listViewItem.getText());
+                        break;
+                    }
+                    case TYPE_EDITTEXT: {
+                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_text_item, null);
+                        TextView textView = (TextView) convertView.findViewById(R.id.TV_text_item);
+                        EditText editText = (EditText) convertView.findViewById(R.id.text_item_value);
+
+                        viewHolder = new ViewHolder(textView, editText);
+                        convertView.setTag(viewHolder);
+                        viewHolder.getText().setText(listViewItem.getText());
+                        viewHolder.getEditText().setHint(listViewItem.getHint());
+                        break;
+                    }
+                    case TYPE_CHECKBOX: {
+                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_checkbox_item, null);
+                        TextView textView = (TextView) convertView.findViewById(R.id.TV_checkbox_item);
+                        CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox_item_checkbox);
+
+                        viewHolder = new ViewHolder(textView, checkBox);
+                        convertView.setTag(viewHolder);
+                        viewHolder.getText().setText(listViewItem.getText());
+                        break;
+                    }
+                    case TYPE_RADIO: {
+                        convertView = LayoutInflater.from(getContext()).inflate(R.layout.vesc_settings_radio_item, null);
+                        TextView textView = (TextView) convertView.findViewById(R.id.TV_radio_item);
+                        RadioGroup radiogroup = (RadioGroup) convertView.findViewById(R.id.radio_item_radio_grouup);
+
+                        String[] radios = objects[position].getRadios();
+                        RadioButton[] radiobutton = new RadioButton[radios.length];
+                        for (int i = 0; i < radios.length; i++) {
+                            radiobutton[i] = new RadioButton(radiogroup.getContext());
+                            radiobutton[i].setText(radios[i]);
+                            radiobutton[i].setId(i);
+                            radiogroup.addView(radiobutton[i]);
+                        }
+
+                        viewHolder = new ViewHolder(textView, radiogroup);
+                        convertView.setTag(viewHolder);
+                        viewHolder.getText().setText(listViewItem.getText());
+                        break;
+                    }
                 }
 
-                convertView.setTag(viewHolder);
-
             } else {
+                convertView.setTag(viewHolder);
                 viewHolder = (ViewHolder) convertView.getTag();
             }
-
-            viewHolder.getText().setText(listViewItem.getText());
-
             return convertView;
         }
 
@@ -182,6 +240,7 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
         TextView text;
         EditText editText;
         CheckBox checkBox;
+        RadioGroup radioGroup;
 
         public ViewHolder(TextView text) {
             this.text = text;
@@ -197,10 +256,14 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
             this.checkBox = checkBox;
         }
 
+        public ViewHolder(TextView text, RadioGroup radioGroup) {
+            this.text = text;
+            this.radioGroup = radioGroup;
+        }
+
         public TextView getText() {
             return text;
         }
-
         public void setText(TextView text) {
             this.text = text;
         }
@@ -208,7 +271,6 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
         public EditText getEditText() {
             return editText;
         }
-
         public void setEditText(EditText editText) {
             this.editText = editText;
         }
@@ -216,9 +278,15 @@ public class Vesc_Settings_Activity extends AppCompatActivity {
         public CheckBox getCheckBox() {
             return checkBox;
         }
-
         public void setCheckBox(CheckBox checkBox) {
             this.checkBox = checkBox;
+        }
+
+        public RadioGroup getRadioGroup() {
+            return radioGroup;
+        }
+        public void setRadioGroup(RadioGroup radioGroup) {
+            this.radioGroup = radioGroup;
         }
     }
 }
